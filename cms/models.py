f29c96841a65ec4543b2c3eef956580c82346eea
Ulz001 <empty3721@163.com>
2023-12-19 12:03:21 +0800

    模型更正

--- cms/models.py
+++ cms/models.py
@@ -11,7 +11,7 @@ class Material(models.Model):
     min_inventory = models.IntegerField(null=False, validators=[MinValueValidator(1)], verbose_name='最小库存')
     now_inventory = models.IntegerField(null=False, validators=[MinValueValidator(0)], verbose_name='当前库存')
     description = models.CharField(max_length=100, null=True, verbose_name='物料描述')
-    picture = models.ImageField(upload_to='/media/upload_images', null=True, verbose_name='物料图片')
+    picture = models.ImageField(upload_to='media/upload_images', null=True, verbose_name='物料图片')
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name='物料价格')
     now_datetime = models.DateTimeField(auto_now=True, verbose_name='入库时间')
     supplier = models.CharField(max_length=15, null=False, verbose_name='供应商')
@@ -30,6 +30,9 @@ class InInventory(models.Model):
 
 
 class OutInventory(models.Model):
+    """
+    出库表
+    """
     material = models.ForeignKey(Material, on_delete=models.CASCADE, related_name='OutInventory',
                                  verbose_name='物料序号')
     out_way = models.CharField(max_length=8, null=False, verbose_name='去向途径')
@@ -39,6 +42,9 @@ class OutInventory(models.Model):
 
 
 class CheckInventory(models.Model):
+    """
+    盘点表
+    """
     material = models.ForeignKey(Material, on_delete=models.CASCADE, verbose_name='物料序号',
                                  related_name='CheckInventory')
     datetime = models.DateTimeField(auto_now_add=True, verbose_name='盘点日期')
