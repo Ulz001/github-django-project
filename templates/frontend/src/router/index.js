2e4e3e36ae23f569b78288d4b86101262a1a316e
Ulz001 <empty3721@163.com>
2023-12-25 20:55:07 +0800

    404页面

--- templates/frontend/src/router/index.js
+++ templates/frontend/src/router/index.js
@@ -18,11 +18,22 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import('../views/404View.vue')
     }
   ]
 })
 
 router.beforeEach((to, from, next) => {
+  if (to.matched.length === 0) {
+    // 如果未匹配到路由
+    from.name ? next({ name: from.name }) : next('/404')
+  } else {
+    next() // 如果匹配到正确跳转
+  }
   const userStore = useUserStore()
   const { status, username, token } = userStore.getUserData()
   if (to.name === 'login' || to.name === 'register') {
---
+++ templates/frontend/src/views/404View.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-wrapper">
+    <div class="wscn-http404-container">
+      <div class="wscn-http404">
+        <div class="pic-404">
+          <img class="pic-404__parent" src="./images/404.png" alt="404" />
+          <img class="pic-404__child left" src="./images/404_cloud.png" alt="404" />
+          <img class="pic-404__child mid" src="./images/404_cloud.png" alt="404" />
+          <img class="pic-404__child right" src="./images/404_cloud.png" alt="404" />
+        </div>
+        <div class="bullshit">
+          <div class="bullshit__oops">努力更新中，敬请期待...</div>
+          <!--          <div class="bullshit__headline">{{ message }}</div>-->
+          <div class="bullshit__info">您可以点击下面按钮返回</div>
+          <div @click="handleBack" class="bullshit__return-home">返回</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page404',
+  computed: {
+    message() {
+      return '没有找到你要访问的页面。。。'
+    }
+  },
+  methods: {
+    handleBack() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
